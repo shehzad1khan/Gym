f5d9bd8b3bbd84ade91ec47a667c62b1a2fc31ea
shehzad1khan <commonemail61@gmail.com>
2022-06-03 22:52:09 +0500

    images downloaded

--- login.php
+++ login.php
@@ -62,8 +62,8 @@ include('database.php');
         <div class="card" style="border-radius: 1rem; background:#5b50b2">
           <div class="row g-0">
             <div class="col-md-6 col-lg-6 d-none d-md-block">
-              <img src="images/img1.jpg"
-                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; height:630px" />
+              <img src="images/gym5.jpg"
+                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; width:500px; height:560px" />
             </div>
             <div class="col-md-6 col-lg-6 d-flex align-items-center">
               <div class="card-body p-4 p-lg-5 text-black">
